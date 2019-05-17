25b63a2f3537cb977036ff7e4720f75f555d0b0c
Chase Wex <clapingmanly@hotmail.com>
2019-05-17 12:43:41 -0400

    Created a basic list to show info

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -3,8 +3,13 @@ import React from 'react';
 export function SmurfList(props)
 {
     return(
-        <div>
-            {props.smurfs.map( x=> <div>{x.name}</div>)}
+        <div >
+            {props.smurfs.map( x=> <div style={{display: "flex", justifyContent: "space-around"}} key={x.id}>
+                <div>{x.name}</div>
+                <div>{x.age}</div>
+                <div>{x.height}</div>
+                <button onClick={()=> props.delete(x.id)}>x</button>
+            </div>)}
         </div>
     )
 }
